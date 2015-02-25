edba9af70c38ff8200fa327ddc7f3c0068b6d0fc
Sergio Gonzalez <alu0100354256@ull.edu.es>
2015-02-25 15:19:36 +0000

    Correcciones

--- conf.js
+++ conf.js
@@ -11,6 +11,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
        'test/chai.js',
+       'test/mocha.js',
        'index.html',
        'test/index.html',
        'test-main.js',
